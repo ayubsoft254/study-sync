69adcead505b732b2989799920b85178288ab2fd
Ayubsoft254 <henryoayub15@gmail.com>
2024-10-06 09:21:11 +0300

    "Added WebSocket support to ASGI application and configured Channels with Redis backend"

--- src/home/settings.py
+++ src/home/settings.py
@@ -114,6 +114,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
