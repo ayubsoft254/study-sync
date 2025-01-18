168067e31540572d3e73ea310ec0504488ccb017
ayubsoft254 <henryoayub15@gmail.com>
2025-01-18 07:47:28 +0300

    Register Mentor, Mentee, and MentorRating models in the admin interface

--- src/app/admin.py
+++ src/app/admin.py
@@ -3,14 +3,18 @@ from .models import *
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Mentor)
+admin.site.register(Mentee)
 admin.site.register(School)
 admin.site.register(Course)
-admin.site.register(Mentee)
 admin.site.register(Resource)
 admin.site.register(Chat)
 admin.site.register(Message)
 admin.site.register(Call)
 admin.site.register(MentorSession)
 admin.site.register(SessionAttendance)
+admin.site.register(MentorRating)
+
+
 
 
