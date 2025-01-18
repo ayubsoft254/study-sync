ef3be877e8520e0597a3246968a3d4be042eca85
ayubsoft254 <henryoayub15@gmail.com>
2025-01-18 07:02:51 +0300

    Register models in the admin interface for user, school, course, mentee, resource, chat, message, call, mentor session, and session attendance

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(School)
+admin.site.register(Course)
+admin.site.register(Mentee)
+admin.site.register(Resource)
+admin.site.register(Chat)
+admin.site.register(Message)
+admin.site.register(Call)
+admin.site.register(MentorSession)
+admin.site.register(SessionAttendance)
+
+
